10e7839ea150b25535333809d712751b987cd386
Felix Angelov <felangelov@gmail.com>
2021-01-31 13:45:25 -0600

    feat: create VeryGoodCommandRunner (#4)

--- bin/very_good.dart
+++ bin/very_good.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'dart:io';
+import 'package:very_good_cli/src/command_runner.dart';
+
+void main(List<String> args) async {
+  await _flushThenExit(await VeryGoodCommandRunner().run(args));
+}
+
+/// Flushes the stdout and stderr streams, then exits the program with the given
+/// status code.
+///
+/// This returns a Future that will never complete, since the program will have
+/// exited already. This is useful to prevent Future chains from proceeding
+/// after you've decided to exit.
+Future _flushThenExit(int status) {
+  return Future.wait<void>([stdout.close(), stderr.close()])
+      .then<void>((_) => exit(status));
+}
