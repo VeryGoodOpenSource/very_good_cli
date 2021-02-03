3b35c4e7c465368f977d83dacecc4b8ffd631ad1
VGV <vgvbot@users.noreply.github.com>
2021-02-03 05:01:37 +0000

    chore: generate very_good_core template

--- bricks/very_good_core/__brick__/lib/app/app_bloc_observer.dart
+++ bricks/very_good_core/__brick__/lib/app/app_bloc_observer.dart
@@ -1,16 +1,17 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
-import 'package:flutter/foundation.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    debugPrint('onTransition(${bloc.runtimeType}, $transition)');
+    log('onTransition(${bloc.runtimeType}, $transition)');
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    debugPrint('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
 }
--- bricks/very_good_core/__brick__/lib/main_development.dart
+++ bricks/very_good_core/__brick__/lib/main_development.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/widgets.dart';
 import 'package:bloc/bloc.dart';
 import 'package:{{project_name}}/app/app.dart';
@@ -5,5 +8,12 @@ import 'package:{{project_name}}/app/app_bloc_observer.dart';
 
 void main() {
   Bloc.observer = AppBlocObserver();
-  runApp(const App());
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  runZonedGuarded(
+    () => runApp(const App()),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
 }
