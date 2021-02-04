db0bd47ec6a9530110fdf4e6014ba1569441662b
Felix Angelov <felangelov@gmail.com>
2021-02-04 16:41:12 -0600

    feat(very_good_core): add LICENSE and copyright header

--- tool/generator/main.dart
+++ tool/generator/main.dart
@@ -8,6 +8,15 @@ final _targetPath = path.join(
   '__brick__',
 );
 
+const copyrightHeader = '''
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+// 
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+''';
+
 void main() async {
   // Remove Previously Generated Files
   final targetDir = Directory(_targetPath);
@@ -27,6 +36,11 @@ void main() async {
       var file = _;
 
       try {
+        if (path.extension(file.path) == '.dart') {
+          final contents = await file.readAsString();
+          file = await file.writeAsString('$copyrightHeader\n$contents');
+        }
+
         final contents = await file.readAsString();
         file = await file.writeAsString(
           contents
@@ -37,19 +51,17 @@ void main() async {
                 '{{#titleCase}}{{project_name}}{{/titleCase}}',
               ),
         );
+        final fileSegments = file.path.split('/').sublist(3);
+        if (fileSegments.contains('very_good_core')) {
+          final newPathSegment = fileSegments
+              .join('/')
+              .replaceAll('very_good_core', '{{project_name}}');
+          final newPath = path.join(_targetPath, newPathSegment);
+          File(newPath).createSync(recursive: true);
+          file.renameSync(newPath);
+          Directory(file.parent.path).deleteSync(recursive: true);
+        }
       } catch (_) {}
-
-      final fileSegments = file.path.split('/').sublist(3);
-      if (fileSegments.contains('very_good_core')) {
-        final newPathSegment = fileSegments.join('/').replaceAll(
-              'very_good_core',
-              '{{project_name}}',
-            );
-        final newPath = path.join(_targetPath, newPathSegment);
-        File(newPath).createSync(recursive: true);
-        file.renameSync(newPath);
-        Directory(file.parent.path).deleteSync(recursive: true);
-      }
     }),
   );
 }
