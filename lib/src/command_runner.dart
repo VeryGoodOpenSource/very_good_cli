6c142abb0cee37e60b395a3d4a6be1114b6eb597
Felix Angelov <felangelov@gmail.com>
2021-02-16 15:01:50 -0600

    feat: add usage analytics

--- lib/src/command_runner.dart
+++ lib/src/command_runner.dart
@@ -1,32 +1,65 @@
 import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
+import 'package:io/ansi.dart';
 import 'package:io/io.dart';
 import 'package:mason/mason.dart';
+import 'package:usage/usage_io.dart';
 import 'package:very_good_cli/src/commands/commands.dart';
 
 import 'version.dart';
+
+// The Google Analytics tracking ID.
+const _gaTrackingId = 'UA-117465969-4';
+
+// The Google Analytics Application Name.
+const _gaAppName = 'very-good-cli';
 
 /// {@template very_good_command_runner}
 /// A [CommandRunner] for the Very Good CLI.
 /// {@endtemplate}
 class VeryGoodCommandRunner extends CommandRunner<int> {
   /// {@macro very_good_command_runner}
-  VeryGoodCommandRunner({Logger logger})
+  VeryGoodCommandRunner({Analytics analytics, Logger logger})
       : _logger = logger ?? Logger(),
+        _analytics =
+            analytics ?? AnalyticsIO(_gaTrackingId, _gaAppName, packageVersion),
         super('very_good', '🦄 A Very Good Command Line Interface') {
-    argParser.addFlag(
-      'version',
-      negatable: false,
-      help: 'Print the current version.',
-    );
-    addCommand(CreateCommand(logger: logger));
+    argParser
+      ..addFlag(
+        'version',
+        negatable: false,
+        help: 'Print the current version.',
+      )
+      ..addOption(
+        'analytics',
+        help: 'Opt into or out of anonymous usage statistics.',
+      );
+    addCommand(CreateCommand(analytics: _analytics, logger: logger));
   }
 
+  /// Standard timeout duration for the CLI.
+  static const timeout = Duration(milliseconds: 500);
+
   final Logger _logger;
+  final Analytics _analytics;
 
   @override
   Future<int> run(Iterable<String> args) async {
     try {
+      if (_analytics.firstRun) {
+        final response = _logger.prompt(lightGray.wrap(
+          '''+---------------------------------------------------+
+|           Welcome to the Very Good CLI!           |
++---------------------------------------------------+
+| We would like to collect anonymous                |
+| usage statistics in order to improve the tool.    |
+| Would you like to opt-into help us improve? [y/n] |
++---------------------------------------------------+\n''',
+        ));
+        final normalizedResponse = response.toLowerCase().trim();
+        _analytics.enabled =
+            normalizedResponse == 'y' || normalizedResponse == 'yes';
+      }
       final _argResults = parse(args);
       return await runCommand(_argResults) ?? ExitCode.success.code;
     } on FormatException catch (e, stackTrace) {
@@ -50,6 +83,12 @@ class VeryGoodCommandRunner extends CommandRunner<int> {
   Future<int> runCommand(ArgResults topLevelResults) async {
     if (topLevelResults['version'] == true) {
       _logger.info('very_good version: $packageVersion');
+      return ExitCode.success.code;
+    }
+    if (topLevelResults['analytics'] != null) {
+      final optIn = topLevelResults['analytics'] == 'true' ? true : false;
+      _analytics.enabled = optIn;
+      _logger.info('analytics ${_analytics.enabled ? 'enabled' : 'disabled'}.');
       return ExitCode.success.code;
     }
     return super.runCommand(topLevelResults);
--- test/src/commands/create_test.dart
+++ test/src/commands/create_test.dart
@@ -4,10 +4,13 @@ import 'package:io/io.dart';
 import 'package:mason/mason.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:usage/usage_io.dart';
 import 'package:very_good_cli/src/command_runner.dart';
 import 'package:very_good_cli/src/commands/create.dart';
 
 class MockArgResults extends Mock implements ArgResults {}
+
+class MockAnalytics extends Mock implements Analytics {}
 
 class MockLogger extends Mock implements Logger {}
 
@@ -15,17 +18,36 @@ class MockMasonGenerator extends Mock implements MasonGenerator {}
 
 void main() {
   group('Create', () {
+    Analytics analytics;
     Logger logger;
     VeryGoodCommandRunner commandRunner;
 
     setUp(() {
+      analytics = MockAnalytics();
+      when(analytics.firstRun).thenReturn(false);
+      when(analytics.enabled).thenReturn(false);
+      when(analytics.sendEvent(any, any, label: anyNamed('label')))
+          .thenAnswer((_) => Future.value());
+      when(analytics.waitForLastPing(timeout: anyNamed('timeout')))
+          .thenAnswer((_) => Future.value());
+
       logger = MockLogger();
       when(logger.progress(any)).thenReturn(([_]) {});
-      commandRunner = VeryGoodCommandRunner(logger: logger);
+      commandRunner = VeryGoodCommandRunner(
+        analytics: analytics,
+        logger: logger,
+      );
     });
 
-    test('can be instantiated without any explicit dependencies', () {
-      final command = CreateCommand();
+    test('throws AssertionError when analytics is null', () {
+      expect(
+        () => CreateCommand(analytics: null),
+        throwsA(isA<AssertionError>()),
+      );
+    });
+
+    test('can be instantiated without explicit logger', () {
+      final command = CreateCommand(analytics: analytics);
       expect(command, isNotNull);
     });
 
@@ -69,11 +91,14 @@ void main() {
       final argResults = MockArgResults();
       final generator = MockMasonGenerator();
       final command = CreateCommand(
+        analytics: analytics,
         logger: logger,
         generator: (_) async => generator,
       )..argResultOverrides = argResults;
       when(argResults['project-name']).thenReturn('my_app');
       when(argResults.rest).thenReturn(['.tmp']);
+      when(generator.id).thenReturn('generator_id');
+      when(generator.description).thenReturn('generator description');
       when(generator.generate(any, vars: anyNamed('vars')))
           .thenAnswer((_) async => 62);
       final result = await command.run();
@@ -95,6 +120,16 @@ void main() {
           ),
           vars: {'project_name': 'my_app'},
         ),
+      ).called(1);
+      verify(
+        analytics.sendEvent(
+          'create',
+          'generator_id',
+          label: 'generator description',
+        ),
+      ).called(1);
+      verify(
+        analytics.waitForLastPing(timeout: VeryGoodCommandRunner.timeout),
       ).called(1);
     });
   });
