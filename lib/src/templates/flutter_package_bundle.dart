0e63c521c46811a21e8f0b48e48ac6a78070f257
Dennis Angelov <denangelov123@gmail.com>
2021-07-20 15:28:00 -0500

    formatting

--- lib/src/templates/flutter_package_bundle.dart
+++ lib/src/templates/flutter_package_bundle.dart
@@ -3,4 +3,52 @@
 
 import 'package:mason/mason.dart';
 
-final flutterPackageBundle = MasonBundle.fromJson({"files":[{"path":".gitignore","data":"IyBNaXNjZWxsYW5lb3VzCiouY2xhc3MKKi5sb2cKKi5weWMKKi5zd3AKLkRTX1N0b3JlCi5hdG9tLwouYnVpbGRsb2cvCi5oaXN0b3J5Ci5zdm4vCgojIEludGVsbGlKIHJlbGF0ZWQKKi5pbWwKKi5pcHIKKi5pd3MKLmlkZWEvCgojIFZTQ29kZSByZWxhdGVkCi52c2NvZGUvCgojIEZsdXR0ZXIvRGFydC9QdWIgcmVsYXRlZAoqKi9kb2MvYXBpLwoqKi9pb3MvRmx1dHRlci8ubGFzdF9idWlsZF9pZAouZGFydF90b29sLwouZmx1dHRlci1wbHVnaW5zCi5mbHV0dGVyLXBsdWdpbnMtZGVwZW5kZW5jaWVzCi5wYWNrYWdlcwoucHViLWNhY2hlLwoucHViLwovYnVpbGQvCgojIFdlYiByZWxhdGVkCmxpYi9nZW5lcmF0ZWRfcGx1Z2luX3JlZ2lzdHJhbnQuZGFydAoKIyBTeW1ib2xpY2F0aW9uIHJlbGF0ZWQKYXBwLiouc3ltYm9scwoKIyBPYmZ1c2NhdGlvbiByZWxhdGVkCmFwcC4qLm1hcC5qc29uCg==","type":"text"},{"path":"{{project_name}}/test/{{project_name}}_test.dart","data":"Ly8gaWdub3JlX2Zvcl9maWxlOiBwcmVmZXJfY29uc3RfY29uc3RydWN0b3JzCmltcG9ydCAncGFja2FnZTpmbHV0dGVyX3Rlc3QvZmx1dHRlcl90ZXN0LmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6e3twcm9qZWN0X25hbWV9fS97e3Byb2plY3RfbmFtZX19LmRhcnQnOwoKdm9pZCBtYWluKCkgewogIGdyb3VwKCd7eyMgcGFzY2FsQ2FzZX19e3twcm9qZWN0X25hbWV9fXt7LyBwYXNjYWxDYXNlfX0nLCAoKSB7CiAgICB0ZXN0KCdjYW4gYmUgaW5zdGFudGlhdGVkJywgKCkgewogICAgICBleHBlY3Qoe3sjIHBhc2NhbENhc2V9fXt7cHJvamVjdF9uYW1lfX17ey8gcGFzY2FsQ2FzZX19KCksIGlzTm90TnVsbCk7CiAgICB9KTsKICB9KTsKfQ==","type":"text"},{"path":"{{project_name}}/README.md","data":"IyB7e3Byb2plY3RfbmFtZX19CgpbIVtzdHlsZTogdmVyeSBnb29kIGFuYWx5c2lzXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua10KWyFbTGljZW5zZTogTUlUXVtsaWNlbnNlX2JhZGdlXV1bbGljZW5zZV9saW5rXQoKe3tkZXNjcmlwdGlvbn19CgpbbGljZW5zZV9iYWRnZV06IGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vYmFkZ2UvbGljZW5zZS1NSVQtYmx1ZS5zdmcKW2xpY2Vuc2VfbGlua106IGh0dHBzOi8vb3BlbnNvdXJjZS5vcmcvbGljZW5zZXMvTUlUClt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdOiBodHRwczovL2ltZy5zaGllbGRzLmlvL2JhZGdlL3N0eWxlLXZlcnlfZ29vZF9hbmFseXNpcy1CMjJDODkuc3ZnClt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua106IGh0dHBzOi8vcHViLmRldi9wYWNrYWdlcy92ZXJ5X2dvb2RfYW5hbHlzaXM=","type":"text"},{"path":"{{project_name}}/pubspec.yaml","data":"bmFtZToge3twcm9qZWN0X25hbWV9fQpkZXNjcmlwdGlvbjoge3tkZXNjcmlwdGlvbn19CnZlcnNpb246IDEuMC4wKzEKcHVibGlzaF90bzogbm9uZQoKZW52aXJvbm1lbnQ6CiAgc2RrOiAiPj0yLjEyLjAgPDMuMC4wIgoKZGVwZW5kZW5jaWVzOgogIGZsdXR0ZXI6CiAgICBzZGs6IGZsdXR0ZXIKCmRldl9kZXBlbmRlbmNpZXM6CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgdmVyeV9nb29kX2FuYWx5c2lzOiBeMi4xLjA=","type":"text"},{"path":"{{project_name}}/lib/{{project_name}}.dart","data":"bGlicmFyeSB7e3Byb2plY3RfbmFtZX19OwoKZXhwb3J0ICdzcmMve3twcm9qZWN0X25hbWV9fS5kYXJ0Jzs=","type":"text"},{"path":"{{project_name}}/lib/src/{{project_name}}.dart","data":"Ly8vIHtAdGVtcGxhdGUge3twcm9qZWN0X25hbWV9fX0KLy8vIHt7ZGVzY3JpcHRpb259fQovLy8ge0BlbmR0ZW1wbGF0ZX0KY2xhc3Mge3sjIHBhc2NhbENhc2V9fXt7cHJvamVjdF9uYW1lfX17ey8gcGFzY2FsQ2FzZX19IHsKICAvLy8ge0BtYWNybyB7e3Byb2plY3RfbmFtZX19fQogIGNvbnN0IHt7IyBwYXNjYWxDYXNlfX17e3Byb2plY3RfbmFtZX19e3svIHBhc2NhbENhc2V9fSgpOwp9","type":"text"},{"path":"{{project_name}}/analysis_options.yaml","data":"aW5jbHVkZTogcGFja2FnZTp2ZXJ5X2dvb2RfYW5hbHlzaXMvYW5hbHlzaXNfb3B0aW9ucy55YW1s","type":"text"}],"name":"flutter_package","description":"A new Flutter Package","vars":["project_name","description"]});
+final flutterPackageBundle = MasonBundle.fromJson({
+  "files": [
+    {
+      "path": ".gitignore",
+      "data":
+          "IyBNaXNjZWxsYW5lb3VzCiouY2xhc3MKKi5sb2cKKi5weWMKKi5zd3AKLkRTX1N0b3JlCi5hdG9tLwouYnVpbGRsb2cvCi5oaXN0b3J5Ci5zdm4vCgojIEludGVsbGlKIHJlbGF0ZWQKKi5pbWwKKi5pcHIKKi5pd3MKLmlkZWEvCgojIFZTQ29kZSByZWxhdGVkCi52c2NvZGUvCgojIEZsdXR0ZXIvRGFydC9QdWIgcmVsYXRlZAoqKi9kb2MvYXBpLwoqKi9pb3MvRmx1dHRlci8ubGFzdF9idWlsZF9pZAouZGFydF90b29sLwouZmx1dHRlci1wbHVnaW5zCi5mbHV0dGVyLXBsdWdpbnMtZGVwZW5kZW5jaWVzCi5wYWNrYWdlcwoucHViLWNhY2hlLwoucHViLwovYnVpbGQvCgojIFdlYiByZWxhdGVkCmxpYi9nZW5lcmF0ZWRfcGx1Z2luX3JlZ2lzdHJhbnQuZGFydAoKIyBTeW1ib2xpY2F0aW9uIHJlbGF0ZWQKYXBwLiouc3ltYm9scwoKIyBPYmZ1c2NhdGlvbiByZWxhdGVkCmFwcC4qLm1hcC5qc29uCg==",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/test/{{project_name}}_test.dart",
+      "data":
+          "Ly8gaWdub3JlX2Zvcl9maWxlOiBwcmVmZXJfY29uc3RfY29uc3RydWN0b3JzCmltcG9ydCAncGFja2FnZTpmbHV0dGVyX3Rlc3QvZmx1dHRlcl90ZXN0LmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6e3twcm9qZWN0X25hbWV9fS97e3Byb2plY3RfbmFtZX19LmRhcnQnOwoKdm9pZCBtYWluKCkgewogIGdyb3VwKCd7eyMgcGFzY2FsQ2FzZX19e3twcm9qZWN0X25hbWV9fXt7LyBwYXNjYWxDYXNlfX0nLCAoKSB7CiAgICB0ZXN0KCdjYW4gYmUgaW5zdGFudGlhdGVkJywgKCkgewogICAgICBleHBlY3Qoe3sjIHBhc2NhbENhc2V9fXt7cHJvamVjdF9uYW1lfX17ey8gcGFzY2FsQ2FzZX19KCksIGlzTm90TnVsbCk7CiAgICB9KTsKICB9KTsKfQ==",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/README.md",
+      "data":
+          "IyB7e3Byb2plY3RfbmFtZX19CgpbIVtzdHlsZTogdmVyeSBnb29kIGFuYWx5c2lzXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua10KWyFbTGljZW5zZTogTUlUXVtsaWNlbnNlX2JhZGdlXV1bbGljZW5zZV9saW5rXQoKe3tkZXNjcmlwdGlvbn19CgpbbGljZW5zZV9iYWRnZV06IGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vYmFkZ2UvbGljZW5zZS1NSVQtYmx1ZS5zdmcKW2xpY2Vuc2VfbGlua106IGh0dHBzOi8vb3BlbnNvdXJjZS5vcmcvbGljZW5zZXMvTUlUClt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdOiBodHRwczovL2ltZy5zaGllbGRzLmlvL2JhZGdlL3N0eWxlLXZlcnlfZ29vZF9hbmFseXNpcy1CMjJDODkuc3ZnClt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua106IGh0dHBzOi8vcHViLmRldi9wYWNrYWdlcy92ZXJ5X2dvb2RfYW5hbHlzaXM=",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/pubspec.yaml",
+      "data":
+          "bmFtZToge3twcm9qZWN0X25hbWV9fQpkZXNjcmlwdGlvbjoge3tkZXNjcmlwdGlvbn19CnZlcnNpb246IDEuMC4wKzEKcHVibGlzaF90bzogbm9uZQoKZW52aXJvbm1lbnQ6CiAgc2RrOiAiPj0yLjEyLjAgPDMuMC4wIgoKZGVwZW5kZW5jaWVzOgogIGZsdXR0ZXI6CiAgICBzZGs6IGZsdXR0ZXIKCmRldl9kZXBlbmRlbmNpZXM6CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgdmVyeV9nb29kX2FuYWx5c2lzOiBeMi4xLjA=",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/lib/{{project_name}}.dart",
+      "data":
+          "bGlicmFyeSB7e3Byb2plY3RfbmFtZX19OwoKZXhwb3J0ICdzcmMve3twcm9qZWN0X25hbWV9fS5kYXJ0Jzs=",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/lib/src/{{project_name}}.dart",
+      "data":
+          "Ly8vIHtAdGVtcGxhdGUge3twcm9qZWN0X25hbWV9fX0KLy8vIHt7ZGVzY3JpcHRpb259fQovLy8ge0BlbmR0ZW1wbGF0ZX0KY2xhc3Mge3sjIHBhc2NhbENhc2V9fXt7cHJvamVjdF9uYW1lfX17ey8gcGFzY2FsQ2FzZX19IHsKICAvLy8ge0BtYWNybyB7e3Byb2plY3RfbmFtZX19fQogIGNvbnN0IHt7IyBwYXNjYWxDYXNlfX17e3Byb2plY3RfbmFtZX19e3svIHBhc2NhbENhc2V9fSgpOwp9",
+      "type": "text"
+    },
+    {
+      "path": "{{project_name}}/analysis_options.yaml",
+      "data":
+          "aW5jbHVkZTogcGFja2FnZTp2ZXJ5X2dvb2RfYW5hbHlzaXMvYW5hbHlzaXNfb3B0aW9ucy55YW1s",
+      "type": "text"
+    }
+  ],
+  "name": "flutter_package",
+  "description": "A new Flutter Package",
+  "vars": ["project_name", "description"]
+});
