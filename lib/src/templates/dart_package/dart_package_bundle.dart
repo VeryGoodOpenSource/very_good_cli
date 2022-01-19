6612ec73cce8c995eb66b20ac52e32f18e18c92a
Felix Angelov <felangelov@gmail.com>
2022-01-19 11:53:08 -0600

    refactor(dart_package): upgrade to mason v0.1.0-dev.4

--- lib/src/templates/dart_package/dart_package_bundle.dart
+++ lib/src/templates/dart_package/dart_package_bundle.dart
@@ -1,28 +1,10 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars
+// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars, implicit_dynamic_list_literal, implicit_dynamic_map_literal
 
 import 'package:mason/mason.dart';
 
 final dartPackageBundle = MasonBundle.fromJson(<String, dynamic>{
   "files": [
-    {
-      "path": "test/src/{{project_name}}_test.dart",
-      "data":
-          "Ly8gaWdub3JlX2Zvcl9maWxlOiBwcmVmZXJfY29uc3RfY29uc3RydWN0b3JzCmltcG9ydCAncGFja2FnZTp0ZXN0L3Rlc3QuZGFydCc7CmltcG9ydCAncGFja2FnZTp7e3Byb2plY3RfbmFtZX19L3t7cHJvamVjdF9uYW1lfX0uZGFydCc7Cgp2b2lkIG1haW4oKSB7CiAgZ3JvdXAoJ3t7IyBwYXNjYWxDYXNlfX17e3Byb2plY3RfbmFtZX19e3svIHBhc2NhbENhc2V9fScsICgpIHsKICAgIHRlc3QoJ2NhbiBiZSBpbnN0YW50aWF0ZWQnLCAoKSB7CiAgICAgIGV4cGVjdCh7eyMgcGFzY2FsQ2FzZX19e3twcm9qZWN0X25hbWV9fXt7LyBwYXNjYWxDYXNlfX0oKSwgaXNOb3ROdWxsKTsKICAgIH0pOwogIH0pOwp9Cg==",
-      "type": "text"
-    },
-    {
-      "path": "README.md",
-      "data":
-          "IyB7e3Byb2plY3RfbmFtZX19CgpbIVtzdHlsZTogdmVyeSBnb29kIGFuYWx5c2lzXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua10KWyFbTGljZW5zZTogTUlUXVtsaWNlbnNlX2JhZGdlXV1bbGljZW5zZV9saW5rXQoKe3tkZXNjcmlwdGlvbn19CgpbbGljZW5zZV9iYWRnZV06IGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vYmFkZ2UvbGljZW5zZS1NSVQtYmx1ZS5zdmcKW2xpY2Vuc2VfbGlua106IGh0dHBzOi8vb3BlbnNvdXJjZS5vcmcvbGljZW5zZXMvTUlUClt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdOiBodHRwczovL2ltZy5zaGllbGRzLmlvL2JhZGdlL3N0eWxlLXZlcnlfZ29vZF9hbmFseXNpcy1CMjJDODkuc3ZnClt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua106IGh0dHBzOi8vcHViLmRldi9wYWNrYWdlcy92ZXJ5X2dvb2RfYW5hbHlzaXM=",
-      "type": "text"
-    },
-    {
-      "path": "pubspec.yaml",
-      "data":
-          "bmFtZToge3twcm9qZWN0X25hbWV9fQpkZXNjcmlwdGlvbjoge3tkZXNjcmlwdGlvbn19CnZlcnNpb246IDEuMC4wKzEKcHVibGlzaF90bzogbm9uZQoKZW52aXJvbm1lbnQ6CiAgc2RrOiAiPj0yLjE0LjAgPDMuMC4wIgoKZGV2X2RlcGVuZGVuY2llczoKICB0ZXN0OiBeMS4xOS4yCiAgdmVyeV9nb29kX2FuYWx5c2lzOiBeMi40LjAK",
-      "type": "text"
-    },
     {
       "path": ".gitignore",
       "data":
@@ -30,9 +12,9 @@ final dartPackageBundle = MasonBundle.fromJson(<String, dynamic>{
       "type": "text"
     },
     {
-      "path": "lib/{{project_name}}.dart",
+      "path": "analysis_options.yaml",
       "data":
-          "bGlicmFyeSB7e3Byb2plY3RfbmFtZX19OwoKZXhwb3J0ICdzcmMve3twcm9qZWN0X25hbWV9fS5kYXJ0JzsK",
+          "aW5jbHVkZTogcGFja2FnZTp2ZXJ5X2dvb2RfYW5hbHlzaXMvYW5hbHlzaXNfb3B0aW9ucy4yLjQuMC55YW1s",
       "type": "text"
     },
     {
@@ -42,13 +24,46 @@ final dartPackageBundle = MasonBundle.fromJson(<String, dynamic>{
       "type": "text"
     },
     {
-      "path": "analysis_options.yaml",
+      "path": "lib/{{project_name}}.dart",
       "data":
-          "aW5jbHVkZTogcGFja2FnZTp2ZXJ5X2dvb2RfYW5hbHlzaXMvYW5hbHlzaXNfb3B0aW9ucy4yLjQuMC55YW1s",
+          "bGlicmFyeSB7e3Byb2plY3RfbmFtZX19OwoKZXhwb3J0ICdzcmMve3twcm9qZWN0X25hbWV9fS5kYXJ0JzsK",
+      "type": "text"
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZToge3twcm9qZWN0X25hbWV9fQpkZXNjcmlwdGlvbjoge3tkZXNjcmlwdGlvbn19CnZlcnNpb246IDEuMC4wKzEKcHVibGlzaF90bzogbm9uZQoKZW52aXJvbm1lbnQ6CiAgc2RrOiAiPj0yLjE0LjAgPDMuMC4wIgoKZGV2X2RlcGVuZGVuY2llczoKICB0ZXN0OiBeMS4xOS4yCiAgdmVyeV9nb29kX2FuYWx5c2lzOiBeMi40LjAK",
+      "type": "text"
+    },
+    {
+      "path": "README.md",
+      "data":
+          "IyB7e3Byb2plY3RfbmFtZX19CgpbIVtzdHlsZTogdmVyeSBnb29kIGFuYWx5c2lzXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdXVt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua10KWyFbTGljZW5zZTogTUlUXVtsaWNlbnNlX2JhZGdlXV1bbGljZW5zZV9saW5rXQoKe3tkZXNjcmlwdGlvbn19CgpbbGljZW5zZV9iYWRnZV06IGh0dHBzOi8vaW1nLnNoaWVsZHMuaW8vYmFkZ2UvbGljZW5zZS1NSVQtYmx1ZS5zdmcKW2xpY2Vuc2VfbGlua106IGh0dHBzOi8vb3BlbnNvdXJjZS5vcmcvbGljZW5zZXMvTUlUClt2ZXJ5X2dvb2RfYW5hbHlzaXNfYmFkZ2VdOiBodHRwczovL2ltZy5zaGllbGRzLmlvL2JhZGdlL3N0eWxlLXZlcnlfZ29vZF9hbmFseXNpcy1CMjJDODkuc3ZnClt2ZXJ5X2dvb2RfYW5hbHlzaXNfbGlua106IGh0dHBzOi8vcHViLmRldi9wYWNrYWdlcy92ZXJ5X2dvb2RfYW5hbHlzaXM=",
+      "type": "text"
+    },
+    {
+      "path": "test/src/{{project_name}}_test.dart",
+      "data":
+          "Ly8gaWdub3JlX2Zvcl9maWxlOiBwcmVmZXJfY29uc3RfY29uc3RydWN0b3JzCmltcG9ydCAncGFja2FnZTp0ZXN0L3Rlc3QuZGFydCc7CmltcG9ydCAncGFja2FnZTp7e3Byb2plY3RfbmFtZX19L3t7cHJvamVjdF9uYW1lfX0uZGFydCc7Cgp2b2lkIG1haW4oKSB7CiAgZ3JvdXAoJ3t7IyBwYXNjYWxDYXNlfX17e3Byb2plY3RfbmFtZX19e3svIHBhc2NhbENhc2V9fScsICgpIHsKICAgIHRlc3QoJ2NhbiBiZSBpbnN0YW50aWF0ZWQnLCAoKSB7CiAgICAgIGV4cGVjdCh7eyMgcGFzY2FsQ2FzZX19e3twcm9qZWN0X25hbWV9fXt7LyBwYXNjYWxDYXNlfX0oKSwgaXNOb3ROdWxsKTsKICAgIH0pOwogIH0pOwp9Cg==",
       "type": "text"
     }
   ],
+  "hooks": [],
   "name": "dart_package",
   "description": "A new Dart Package",
-  "vars": ["project_name", "description"]
+  "version": "0.0.1+1",
+  "vars": {
+    "project_name": {
+      "type": "string",
+      "description": "The package name",
+      "default": "my_package",
+      "prompt": "What is the project name?"
+    },
+    "description": {
+      "type": "string",
+      "description": "The package description",
+      "default": "A very good dart package",
+      "prompt": "What is the project description?"
+    }
+  }
 });
