159cfda786bd7d014ccc15f609eba8c33c1ed7e0
Felix Angelov <felangelov@gmail.com>
2022-03-09 13:55:06 -0600

    use published package

--- packages/very_good_test_runner/lib/very_good_test_runner.dart
+++
@@ -1,8 +0,0 @@
-/// {@template very_good_test_runner}
-/// A very good test runner for Flutter and Dart
-/// created by Very Good Ventures 🦄
-/// {@endtemplate}
-library very_good_test_runner;
-
-export 'src/test_event.dart';
-export 'src/very_good_test_runner.dart';
