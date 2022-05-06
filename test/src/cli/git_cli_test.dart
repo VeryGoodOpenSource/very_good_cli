c1cb24cd0c8a1b4b31d789a9cada80ffcb9853f8
Felix Angelov <felangelov@gmail.com>
2022-05-06 13:34:21 -0500

    test: coverage

--- test/src/cli/git_cli_test.dart
+++ test/src/cli/git_cli_test.dart
@@ -23,5 +23,21 @@ void main() {
         );
       });
     });
+
+    group('UnreachableGitDependency', () {
+      test('has correct toString override', () {
+        final remote =
+            Uri.parse('https://github.com/verygoodopensource/_very_good_cli');
+        final exception = UnreachableGitDependency(remote: remote);
+        expect(
+          exception.toString(),
+          equals(
+            '''
+$remote is unreachable.
+Make sure the remote exists and you have the correct access rights.''',
+          ),
+        );
+      });
+    });
   });
 }
