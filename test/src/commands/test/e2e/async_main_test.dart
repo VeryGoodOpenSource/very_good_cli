866bc09f2ed5d64220d2975058530072a6b1818e
Felix Angelov <felangelov@gmail.com>
2022-12-16 14:54:14 -0600

    remove stdout

--- test/src/commands/test/e2e/async_main_test.dart
+++ test/src/commands/test/e2e/async_main_test.dart
@@ -7,24 +7,13 @@ import 'package:universal_io/io.dart';
 import '../../../../helpers/helpers.dart';
 
 void main() {
-  stdout.writeln('main() cwd: "${Directory.current.absolute.path}"');
-  test('sanity', () {
-    stdout.writeln('test() cwd: "${Directory.current.absolute.path}"');
-    expect(true, isTrue);
-  });
-
   test(
     'supports async main methods',
     withRunner((commandRunner, logger, updater, logs) async {
-      stdout.writeln('cwd: "${Directory.current.absolute.path}"');
       final directory = Directory.systemTemp.createTempSync('async_main');
       final fixture = Directory(
         path.join(Directory.current.path, 'test/fixtures/async_main'),
       );
-
-      stdout
-        ..writeln('tempDir: "${directory.absolute.path}"')
-        ..writeln('fixture: "${fixture.absolute.path}"');
 
       await copyDirectory(fixture, directory);
 
