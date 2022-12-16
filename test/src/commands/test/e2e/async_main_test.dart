e7961395c544426bc20c08c01e0c780e8cfb7250
Felix Angelov <felangelov@gmail.com>
2022-12-16 14:00:55 -0600

    testing

--- test/src/commands/test/e2e/async_main_test.dart
+++ test/src/commands/test/e2e/async_main_test.dart
@@ -1,40 +1,43 @@
 @Tags(['e2e'])
-import 'package:mason/mason.dart';
-import 'package:path/path.dart' as path;
+// import 'package:mason/mason.dart';
+// import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 import 'package:universal_io/io.dart';
 
-import '../../../../helpers/helpers.dart';
+// import '../../../../helpers/helpers.dart';
 
 void main() {
   test(
     'supports async main methods',
-    withRunner((commandRunner, logger, updater, logs) async {
+    () {
       stdout.writeln('cwd: "${Directory.current.absolute.path}"');
-      final directory = Directory.systemTemp.createTempSync('async_main');
-      final fixture = Directory(
-        path.join(Directory.current.path, 'test/fixtures/async_main'),
-      );
-      stdout.writeln('fixture: "${fixture.absolute.path}"');
-      await copyDirectory(fixture, directory);
+    },
+    // withRunner((commandRunner, logger, updater, logs) async {
+    //   stdout.writeln('cwd: "${Directory.current.absolute.path}"');
+    //   final directory = Directory.systemTemp.createTempSync('async_main');
+    //   final fixture = Directory(
+    //     path.join(Directory.current.path, 'test/fixtures/async_main'),
+    //   );
+    //   stdout.writeln('fixture: "${fixture.absolute.path}"');
+    //   await copyDirectory(fixture, directory);
 
-      final pubGetResult = await Process.run(
-        'flutter',
-        ['pub', 'get'],
-        workingDirectory: directory.path,
-        runInShell: true,
-      );
+    //   final pubGetResult = await Process.run(
+    //     'flutter',
+    //     ['pub', 'get'],
+    //     workingDirectory: directory.path,
+    //     runInShell: true,
+    //   );
 
-      expect(pubGetResult.exitCode, equals(ExitCode.success.code));
+    //   expect(pubGetResult.exitCode, equals(ExitCode.success.code));
 
-      await IOOverrides.runZoned(
-        () async {
-          final result = await commandRunner.run(['test']);
-          expect(result, equals(ExitCode.success.code));
-        },
-        // getCurrentDirectory: () => directory,
-      );
-    }),
+    //   await IOOverrides.runZoned(
+    //     () async {
+    //       final result = await commandRunner.run(['test']);
+    //       expect(result, equals(ExitCode.success.code));
+    //     },
+    //     // getCurrentDirectory: () => directory,
+    //   );
+    // }),
     timeout: const Timeout(Duration(minutes: 1)),
   );
 }
