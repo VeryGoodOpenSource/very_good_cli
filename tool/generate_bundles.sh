732d452a3330b3cd518dd2f2dda086dee4587fa4
Renan <6718144+renancaraujo@users.noreply.github.com>
2023-02-03 21:49:21 +0000

    feat: update template bundles (#637)

--- tool/generate_bundles.sh
+++ tool/generate_bundles.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # Runs `mason bundle` to generate bundles for all bricks within the respective templates directories.
 
-bricks=(very_good_core very_good_dart_package very_good_dart_cli very_good_flutter_package very_good_flutter_plugin)
+bricks=(
+    very_good_core
+    very_good_dart_package
+    very_good_dart_cli
+    very_good_flutter_package
+    very_good_flutter_plugin
+    very_good_flame_game
+    very_good_docs_site
+)
 
 for brick in "${bricks[@]}"
 do
     echo "bundling $brick..."
-    mason bundle -s git "https://github.com/verygoodopensource/$brick" --git-path brick -t dart -o "lib/src/commands/create/templates/$brick"
+    mason bundle --source hosted $brick --type dart --output-dir "lib/src/commands/create/templates/$brick/"
 done
 
 dart format .
