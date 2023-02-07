110c39eaf9ce2a051d19f826755b32b7a3c28c46
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-02-07 13:36:00 +0000

    create structure

--- bricks/test_optimizer/hooks/test/pre_gen_test.dart
+++ bricks/test_optimizer/hooks/test/pre_gen_test.dart
@@ -1,5 +1,9 @@
+import 'package:test/test.dart';
 
-
-void main(){
-
+void main() {
+  group('Pre gen hook', () {
+    test('Fails when target test dir does not exist', () {});
+    test('Fails when target dir does not contain a pubspec.yaml', () {});
+    test('Completes with test files list', () {});
+  });
 }
