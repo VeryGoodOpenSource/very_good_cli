d54444cea7fe4a449122e93a00716c398b4f827c
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-02-08 13:47:28 +0000

    add prefix

--- lib/src/commands/test/templates/test_optimizer_bundle.dart
+++ lib/src/commands/test/templates/test_optimizer_bundle.dart
@@ -1,3 +1,4 @@
+// To generate this file, run: tools/generate_test_optimizer_bundle.sh
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint, implicit_dynamic_list_literal, implicit_dynamic_map_literal, inference_failure_on_collection_literal
 
