eea5cd1f3f86494dae8688188d5cc8fa924cc154
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-02-08 12:50:26 +0000

    add hook tests

--- bricks/test_optimizer/hooks/pre_gen.dart
+++ bricks/test_optimizer/hooks/pre_gen.dart
@@ -3,19 +3,23 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
 
+typedef ExitFn = Never Function(int code);
+
+ExitFn exitFn = exit;
+
 Future<void> run(HookContext context) async {
   final packageRoot = context.vars['package-root'] as String;
   final testDir = Directory(path.join(packageRoot, 'test'));
 
   if (!testDir.existsSync()) {
     context.logger.err('Could not find directory ${testDir.path}');
-    exit(1);
+    exitFn(1);
   }
 
   final pubspec = File(path.join(packageRoot, 'pubspec.yaml'));
   if (!pubspec.existsSync()) {
     context.logger.err('Could not find pubspec.yaml at ${testDir.path}');
-    exit(1);
+    exitFn(1);
   }
 
   final pubspecContents = await pubspec.readAsString();
@@ -25,8 +29,11 @@ Future<void> run(HookContext context) async {
   final tests = testDir
       .listSync(recursive: true)
       .where((entity) => entity.isTest)
-      .map((entity) =>
-          path.relative(entity.path, from: testDir.path).replaceAll(r'\', '/'))
+      .map(
+        (entity) => path
+            .relative(entity.path, from: testDir.path)
+            .replaceAll(r'\', '/'),
+      )
       .toList();
 
   context.vars = {'tests': tests, 'isFlutter': isFlutter};
--- bricks/test_optimizer/hooks/test/pre_gen_test.dart
+++ bricks/test_optimizer/hooks/test/pre_gen_test.dart
@@ -1,9 +1,145 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
+import '../pre_gen.dart' as pre_gen;
+
+class MockProgress extends Mock implements Progress {}
+
+class MockLogger extends Mock implements Logger {}
+
+class FakeContext extends Fake implements HookContext {
+  @override
+  final logger = MockLogger();
+
+  @override
+  Map<String, Object?> vars = {};
+}
+
 void main() {
+  late HookContext context;
+
+  setUp(() {
+    context = FakeContext();
+  });
+
   group('Pre gen hook', () {
-    test('Fails when target test dir does not exist', () {});
-    test('Fails when target dir does not contain a pubspec.yaml', () {});
-    test('Completes with test files list', () {});
+    group('Completes', () {
+      test('with test files list', () async {
+        final packageRoot =
+            Directory.systemTemp.createTempSync('test_optimizer');
+        File(path.join(packageRoot.path, 'pubspec.yaml')).createSync();
+
+        final testDir = Directory(path.join(packageRoot.path, 'test'))
+          ..createSync();
+        File(path.join(testDir.path, 'test1_test.dart')).createSync();
+        File(path.join(testDir.path, 'test2_test.dart')).createSync();
+        File(path.join(testDir.path, 'no_test_here.dart')).createSync();
+
+        context.vars['package-root'] = packageRoot.absolute.path;
+
+        await pre_gen.run(context);
+
+        expect(context.vars['tests'], [
+          'test2_test.dart',
+          'test1_test.dart',
+        ]);
+        expect(context.vars['isFlutter'], false);
+      });
+
+      test('with proper isFlutter identification', () async {
+        final packageRoot =
+            Directory.systemTemp.createTempSync('test_optimizer');
+
+        File(path.join(packageRoot.path, 'pubspec.yaml'))
+          ..createSync()
+          ..writeAsStringSync('''
+dependencies:
+  flutter:
+    sdk: flutter''');
+
+        Directory(path.join(packageRoot.path, 'test')).createSync();
+
+        context.vars['package-root'] = packageRoot.absolute.path;
+
+        await pre_gen.run(context);
+
+        expect(context.vars['isFlutter'], true);
+      });
+    });
+    group('Fails', () {
+      setUp(() {
+        pre_gen.exitFn = (code) {
+          throw ProcessException('exit', [code.toString()]);
+        };
+      });
+
+      tearDown(() {
+        pre_gen.exitFn = exit;
+      });
+
+      test('when target test dir does not exist', () async {
+        final packageRoot =
+            Directory.systemTemp.createTempSync('test_optimizer');
+        File(path.join(packageRoot.path, 'pubspec.yaml')).createSync();
+
+        final testDir = Directory(path.join(packageRoot.path, 'test'));
+
+        context.vars['package-root'] = packageRoot.absolute.path;
+
+        await expectLater(
+          () => pre_gen.run(context),
+          throwsA(
+            isA<ProcessException>().having(
+              (ex) => ex.arguments.first,
+              'error code',
+              equals('1'),
+            ),
+          ),
+        );
+
+        verify(
+          () => context.logger.err('Could not find directory ${testDir.path}'),
+        ).called(1);
+
+        expect(context.vars['tests'], isNull);
+        expect(context.vars['isFlutter'], isNull);
+      });
+      test('when target dir does not contain a pubspec.yaml', () async {
+        final packageRoot =
+            Directory.systemTemp.createTempSync('test_optimizer');
+
+        final testDir = Directory(path.join(packageRoot.path, 'test'))
+          ..createSync();
+        File(path.join(testDir.path, 'test1_test.dart')).createSync();
+        File(path.join(testDir.path, 'test2_test.dart')).createSync();
+        File(path.join(testDir.path, 'no_test_here.dart')).createSync();
+
+        context.vars['package-root'] = packageRoot.absolute.path;
+
+        await expectLater(
+          () => pre_gen.run(context),
+          throwsA(
+            isA<ProcessException>().having(
+              (ex) => ex.arguments.first,
+              'error code',
+              equals('1'),
+            ),
+          ),
+        );
+
+        verify(
+          () => context.logger.err(
+            'Could not find pubspec.yaml at ${testDir.path}',
+          ),
+        ).called(1);
+
+        expect(context.vars['tests'], isNull);
+        expect(context.vars['isFlutter'], isNull);
+      });
+    });
   });
 }
