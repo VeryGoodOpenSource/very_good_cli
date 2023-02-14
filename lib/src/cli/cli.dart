0d2afccf85d956447747ff6e0352bd473c56efd1
Erick Zanardo <erickzanardoo@gmail.com>
2023-02-14 18:20:13 -0300

    lint

--- lib/src/cli/cli.dart
+++ lib/src/cli/cli.dart
@@ -105,20 +105,20 @@ class _Cmd {
     required bool Function(FileSystemEntity) where,
     String cwd = '.',
   }) {
-    final directories = Directory(cwd).listSync(recursive: true)
-        .where(where).toList()..sort((a, b) {
-          final aSplit = p.split(a.path);
-          final bSplit = p.split(b.path);
-          final aLevel = aSplit.length;
-          final bLevel = bSplit.length;
+    final directories =
+        Directory(cwd).listSync(recursive: true).where(where).toList()
+          ..sort((a, b) {
+            final aSplit = p.split(a.path);
+            final bSplit = p.split(b.path);
+            final aLevel = aSplit.length;
+            final bLevel = bSplit.length;
 
-          if (aLevel == bLevel) {
-            return 1;
-          } else {
-            return aSplit.last.compareTo(bSplit.last);
-          }
-        });
-
+            if (aLevel == bLevel) {
+              return 1;
+            } else {
+              return aSplit.last.compareTo(bSplit.last);
+            }
+          });
 
     return directories.map(run);
   }
