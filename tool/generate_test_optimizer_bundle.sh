a22b7b6ed77e9c3efed06f6673c595c12069b4b5
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-02-22 12:59:23 +0000

    fix: documentation inconsistency with tool directory

--- tool/generate_test_optimizer_bundle.sh
+++ tool/generate_test_optimizer_bundle.sh
@@ -3,6 +3,6 @@
 mason bundle --source path ./bricks/test_optimizer/ -t dart --output-dir lib/src/commands/test/templates
 
 input="lib/src/commands/test/templates/test_optimizer_bundle.dart"
-echo -e "// To generate this file, run: tools/generate_test_optimizer_bundle.sh\n$(cat $input)" > $input
+echo -e "// To generate this file, run: tool/generate_test_optimizer_bundle.sh\n$(cat $input)" > $input
 
 dart format .
