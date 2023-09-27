ea602ff8a0e484ea3de68628f9a3f2e7c5e2edf1
Alejandro Santiago <dev@alestiago.com>
2023-09-27 12:15:06 +0100

    reactoring

--- lib/src/commands/packages/commands/check/commands/licenses.dart
+++ lib/src/commands/packages/commands/check/commands/licenses.dart
@@ -8,6 +8,11 @@ import 'package:path/path.dart' as path;
 import 'package:pub_license/pub_license.dart';
 import 'package:pubspec_lock/pubspec_lock.dart';
 
+/// The basename of the pubspec lock file.
+const _pubspecLockBasename = 'pubspec.lock';
+
+final PubLicense _pubLicense = PubLicense();
+
 /// {@template packages_check_licenses_command}
 /// `very_good packages check licenses` command for checking packages.
 /// {@endtemplate}
@@ -28,8 +33,6 @@ class PackagesCheckLicensesCommand extends Command<int> {
   }
 
   final Logger _logger;
-
-  final PubLicense _pubLicense = PubLicense();
 
   @override
   String get description =>
@@ -53,36 +56,65 @@ class PackagesCheckLicensesCommand extends Command<int> {
     final target = _argResults.rest.length == 1 ? _argResults.rest[0] : '.';
     final targetPath = path.normalize(Directory(target).absolute.path);
 
-    final pubspecLockFile = File(path.join(targetPath, 'pubspec.lock'));
-    if (!pubspecLockFile.existsSync()) {
-      _logger.err('Could not find a pubspec.lock in $targetPath');
+    final pubspecLock = _tryParsePubspecLock(targetPath);
+    if (pubspecLock == null) {
+      return ExitCode.usage.code;
     }
 
-    final pubspecLock =
-        pubspecLockFile.readAsStringSync().loadPubspecLockFromYaml();
-
+    final licenses = <String, SpdxLicense>{};
     for (final dependency in pubspecLock.packages) {
-      final isDirectDependency = dependency.type() == DependencyType.direct;
       // ignore: invalid_use_of_protected_member
       final isPubHostedDepedency = dependency.hosted != null;
+      final isDirectDependency = dependency.type() == DependencyType.direct;
       if (!isDirectDependency || !isPubHostedDepedency) continue;
 
-      final name = dependency.package();
-      late final SpdxLicense license;
-
-      try {
-        license = await _pubLicense.getLicense(name);
-      } on PubLicenseException {
-        _logger.err('Failed to retrieve the license of the package: $name');
-        return ExitCode.unavailable.code;
-      } catch (error) {
-        _logger.err('Unexpected error: $error.');
+      final packageName = dependency.package();
+      final license = await _tryParseLicenseFromPub(packageName);
+      if (license == null) {
         return ExitCode.unavailable.code;
       }
 
-      _logger.info('$name: ${license.value}');
+      _logger.info('$packageName: ${license.value}');
+      licenses[packageName] = license;
     }
 
     return ExitCode.success.code;
+  }
+
+  /// Atttempts to parse a [PubspecLock] file in the given [path].
+  ///
+  /// If no [PubspecLock] file is found or is unable to be parsed, `null` is
+  /// returned and an error is logged accordingly.
+  PubspecLock? _tryParsePubspecLock(String targetPath) {
+    final pubspecLockFile = File(path.join(targetPath, _pubspecLockBasename));
+
+    if (!pubspecLockFile.existsSync()) {
+      _logger.err('Could not find a $_pubspecLockBasename in $targetPath');
+      return null;
+    }
+
+    try {
+      return pubspecLockFile.readAsStringSync().loadPubspecLockFromYaml();
+    } catch (e) {
+      _logger.err('Could not parse $_pubspecLockBasename in $targetPath');
+      return null;
+    }
+  }
+
+  /// Attempts to parse the license of a package registered at pub.dev.
+  ///
+  /// If no license is found or is unable to be parsed, `null` is
+  /// returned and an error is logged accordingly.
+  Future<SpdxLicense?> _tryParseLicenseFromPub(String packageName) async {
+    try {
+      return await _pubLicense.getLicense(packageName);
+    } on PubLicenseException {
+      _logger
+          .err('Failed to retrieve the license of the package: $packageName');
+    } catch (error) {
+      _logger.err('Unexpected error: $error.');
+    }
+
+    return null;
   }
 }
