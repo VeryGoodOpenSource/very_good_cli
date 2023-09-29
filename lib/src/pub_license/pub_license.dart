bf4b6d5ce30283daf9e32315e1e96f3a67a23c9b
Alejandro Santiago <dev@alestiago.com>
2023-09-29 11:24:56 +0100

    included test file

--- lib/src/pub_license/pub_license.dart
+++ lib/src/pub_license/pub_license.dart
@@ -1,7 +1,7 @@
 /// A Dart script which enables checking a package's license.
 ///
 /// The script is intented to be used by Very Good CLI to help extracting
-/// license information. The existance of this script is likely to be ephemeral
+/// license information. The existance of this script is likely to be ephemeral.
 /// It may be obsolete once [pub.dev](https://pub.dev/) exposes stable license
 /// information in their official API; you may track the progress [here](https://github.com/dart-lang/pub-dev/issues/4717).
 library pub_license;
