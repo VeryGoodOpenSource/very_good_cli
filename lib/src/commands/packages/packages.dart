f7832580d8a4e81be0e060c79d35d93b8e91260e
Alejandro Santiago <dev@alestiago.com>
2023-10-04 11:43:28 +0100

    refactor: moved packages get command to its own file (#829)

---
+++ lib/src/commands/packages/packages.dart
@@ -0,0 +1,19 @@
+import 'package:args/command_runner.dart';
+import 'package:mason/mason.dart';
+import 'package:very_good_cli/src/commands/packages/commands/commands.dart';
+
+/// {@template packages_command}
+/// `very_good packages` command for managing packages.
+/// {@endtemplate}
+class PackagesCommand extends Command<int> {
+  /// {@macro packages_command}
+  PackagesCommand({Logger? logger}) {
+    addSubcommand(PackagesGetCommand(logger: logger));
+  }
+
+  @override
+  String get description => 'Command for managing packages.';
+
+  @override
+  String get name => 'packages';
+}
