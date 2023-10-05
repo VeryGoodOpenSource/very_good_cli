6486020251669d333cf89040403f18d73f84c62a
Alejandro Santiago <dev@alestiago.com>
2023-10-05 16:28:22 +0100

    docs

--- lib/src/commands/packages/commands/check/check.dart
+++ lib/src/commands/packages/commands/check/check.dart
@@ -3,7 +3,8 @@ import 'package:mason/mason.dart';
 import 'package:very_good_cli/src/commands/packages/commands/check/commands/commands.dart';
 
 /// {@template packages_check_command}
-/// `very_good packages check` command for checking packages.
+/// `very_good packages check` command for performing checks in a Dart or
+/// Flutter project.
 /// {@endtemplate}
 class PackagesCheckCommand extends Command<int> {
   /// {@macro packages_check_command}
