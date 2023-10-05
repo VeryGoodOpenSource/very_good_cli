88e41bd4f804cacfa9925600c1ce100d5ca9ce74
Alejandro Santiago <dev@alestiago.com>
2023-10-05 18:10:03 +0100

    format

--- lib/src/commands/packages/commands/check/commands/licenses.dart
+++ lib/src/commands/packages/commands/check/commands/licenses.dart
@@ -2,7 +2,6 @@ import 'package:args/command_runner.dart';
 import 'package:mason/mason.dart';
 import 'package:very_good_cli/src/pub_license/pub_license.dart';
 
-
 /// {@template packages_check_licenses_command}
 /// `very_good packages check licenses` command for checking packages licenses.
 /// {@endtemplate}
