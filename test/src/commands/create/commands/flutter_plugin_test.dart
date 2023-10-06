28e00329aaa334d218ff9ce15216c417eec86c6f
droidbg <41873024+droidbg@users.noreply.github.com>
2023-10-06 14:03:30 +0530

    refactor: make all mock class to private (#838)

--- test/src/commands/create/commands/flutter_plugin_test.dart
+++ test/src/commands/create/commands/flutter_plugin_test.dart
@@ -9,17 +9,19 @@ import 'package:very_good_cli/src/commands/commands.dart';
 
 import '../../../../helpers/helpers.dart';
 
-class MockLogger extends Mock implements Logger {}
+class _MockLogger extends Mock implements Logger {}
 
-class MockMasonGenerator extends Mock implements MasonGenerator {}
+class _MockProgress extends Mock implements Progress {}
 
-class MockGeneratorHooks extends Mock implements GeneratorHooks {}
+class _MockMasonGenerator extends Mock implements MasonGenerator {}
 
-class MockArgResults extends Mock implements ArgResults {}
+class _MockGeneratorHooks extends Mock implements GeneratorHooks {}
 
-class FakeLogger extends Fake implements Logger {}
+class _MockArgResults extends Mock implements ArgResults {}
 
-class FakeDirectoryGeneratorTarget extends Fake
+class _FakeLogger extends Fake implements Logger {}
+
+class _FakeDirectoryGeneratorTarget extends Fake
     implements DirectoryGeneratorTarget {}
 
 final expectedUsage = [
@@ -56,14 +58,14 @@ void main() {
   late Logger logger;
 
   setUpAll(() {
-    registerFallbackValue(FakeDirectoryGeneratorTarget());
-    registerFallbackValue(FakeLogger());
+    registerFallbackValue(_FakeDirectoryGeneratorTarget());
+    registerFallbackValue(_FakeLogger());
   });
 
   setUp(() {
-    logger = MockLogger();
+    logger = _MockLogger();
 
-    final progress = MockProgress();
+    final progress = _MockProgress();
 
     when(() => logger.progress(any())).thenReturn(progress);
   });
@@ -115,8 +117,8 @@ void main() {
       late MasonGenerator generator;
 
       setUp(() {
-        hooks = MockGeneratorHooks();
-        generator = MockMasonGenerator();
+        hooks = _MockGeneratorHooks();
+        generator = _MockMasonGenerator();
 
         when(() => generator.hooks).thenReturn(hooks);
         when(
@@ -166,7 +168,7 @@ void main() {
         final tempDirectory = Directory.systemTemp.createTempSync();
         addTearDown(() => tempDirectory.deleteSync(recursive: true));
 
-        final argResults = MockArgResults();
+        final argResults = _MockArgResults();
         final command = CreateFlutterPlugin(
           logger: logger,
           generatorFromBundle: (_) async => throw Exception('oops'),
