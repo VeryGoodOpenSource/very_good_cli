23c644985d96b961fa027600832d173eb7d2f252
Alejandro Santiago <dev@alestiago.com>
2023-10-10 13:51:55 +0100

    improved docs

--- lib/src/commands/packages/commands/check/commands/licenses.dart
+++ lib/src/commands/packages/commands/check/commands/licenses.dart
@@ -118,8 +118,8 @@ class PackagesCheckLicensesCommand extends Command<int> {
 
 /// Attempts to parse a [PubspecLock] file in the given [path].
 ///
-/// If no [PubspecLock] file is found or is unable to be parsed, `null` is
-/// returned.
+/// If no [PubspecLock] file is not readable or is unable to be parsed, `null`
+/// is returned.
 PubspecLock? _tryParsePubspecLock(File pubspecLockFile) {
   try {
     return pubspecLockFile.readAsStringSync().loadPubspecLockFromYaml();
