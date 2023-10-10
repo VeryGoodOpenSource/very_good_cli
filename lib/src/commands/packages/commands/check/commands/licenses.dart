8bcf93445d21af8e100f043d574d56b5416a840c
Alejandro Santiago <dev@alestiago.com>
2023-10-10 13:52:17 +0100

    improved docs

--- lib/src/commands/packages/commands/check/commands/licenses.dart
+++ lib/src/commands/packages/commands/check/commands/licenses.dart
@@ -118,7 +118,7 @@ class PackagesCheckLicensesCommand extends Command<int> {
 
 /// Attempts to parse a [PubspecLock] file in the given [path].
 ///
-/// If no [PubspecLock] file is not readable or is unable to be parsed, `null`
+/// If no [PubspecLock] file is not readable or fails to be parsed, `null`
 /// is returned.
 PubspecLock? _tryParsePubspecLock(File pubspecLockFile) {
   try {
