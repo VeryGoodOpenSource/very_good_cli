beb5178046b8e887c304dd2de67131900643aa1c
Alejandro Santiago <dev@alestiago.com>
2023-10-13 09:49:11 +0100

    update _expectedPackagesCheckLicensesUsage

--- test/src/commands/packages/commands/check/commands/licenses_test.dart
+++ test/src/commands/packages/commands/check/commands/licenses_test.dart
@@ -25,6 +25,8 @@ const _expectedPackagesCheckLicensesUsage = [
       '''          [direct-main] (default)    Check for direct main dependencies.\n'''
       '''          [transitive]               Check for transitive dependencies.\n'''
       '\n'
+      '    --allowed                        Whitelist of allowed licenses.\n'
+      '\n'
       'Run "very_good help" to see global options.'
 ];
 
