83509908036867e1a6033eceb57051d444f639d0
Alejandro Santiago <dev@alestiago.com>
2023-10-13 16:12:45 +0100

    post_gen

--- tool/spdx_license/hooks/post_gen.dart
+++ tool/spdx_license/hooks/post_gen.dart
@@ -1,19 +1,10 @@
-import 'dart:async';
 import 'dart:io';
-
 import 'package:mason/mason.dart';
-import 'package:spdx_license_hooks/hooks.dart';
 
 Future<void> run(HookContext context) async {
-  final runProcess = ProcessOverrides.current?.runProcess ?? Process.run;
-  final result = await runProcess(
-    'dart',
-    ['format'],
-    workingDirectory: Directory.current.path,
-    runInShell: true,
-  );
+  final progress = context.logger.progress('Formatting files');
 
-  if (result.exitCode != ExitCode.success.code) {
-    throw Exception(result.stderr);
-  }
+  await Process.run('dart', ['format', '.']);
+
+  progress.complete();
 }
