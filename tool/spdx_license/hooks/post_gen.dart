e94f9c99a85bc6cfd9c1b423fd3038c5dc642fe8
Alejandro Santiago <dev@alestiago.com>
2023-10-13 16:26:55 +0100

    post_gen update

--- tool/spdx_license/hooks/post_gen.dart
+++ tool/spdx_license/hooks/post_gen.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
 
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Formatting files');
 
   await Process.run(
     'dart',
-    ['format', '.'],
+    ['format', path.join('test', 'spdx_license.gen.dart')],
     workingDirectory: Directory.current.path,
   );
 
