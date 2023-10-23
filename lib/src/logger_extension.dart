a0394bcdf369c8e970787fae20c5269fe4af83e6
Alejandro Santiago <dev@alestiago.com>
2023-10-23 10:28:23 +0100

    fix: avoid failure when terminal has no columns

--- lib/src/logger_extension.dart
+++ lib/src/logger_extension.dart
@@ -1,5 +1,14 @@
 import 'package:mason_logger/mason_logger.dart';
+import 'package:meta/meta.dart';
 import 'package:universal_io/io.dart';
+
+/// The default number of columns for the terminal.
+///
+/// Used when the terminal width cannot be determined. For example, when running
+/// in Azure DevOps, see relevant issue:
+/// https://github.com/VeryGoodOpenSource/very_good_cli/issues/866 .
+@visibleForTesting
+const defaultStdoutTerminalColumns = 80;
 
 /// Extension on the Logger class for custom styled logging.
 extension LoggerX on Logger {
@@ -11,13 +20,22 @@ extension LoggerX on Logger {
   /// Wrap the [text] to fit perfectly within the width of the terminal when
   /// [print]ed.
   ///
-  /// To overwrite the width you can use [length].
+  /// To overwrite the width you can use [length]; otherwise, it defaults to the
+  /// width of the terminal, or to [defaultStdoutTerminalColumns] if the width
+  /// cannot be determined.
   void wrap(
     String? text, {
     required void Function(String?) print,
     int? length,
   }) {
-    final maxLength = length ?? stdout.terminalColumns;
+    late final int maxLength;
+    try {
+      maxLength = length ?? stdout.terminalColumns;
+    } on StdoutException catch (_) {
+      // Not all terminals have a width, so we default to a reasonable value.
+      maxLength = defaultStdoutTerminalColumns;
+    }
+
     for (final sentence in text?.split('/n') ?? <String>[]) {
       final words = sentence.split(' ');
 
