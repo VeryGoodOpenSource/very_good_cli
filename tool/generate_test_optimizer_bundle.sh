8c989fec5be6383abff627a274c6deca3daab023
Alejandro Santiago <dev@alestiago.com>
2023-12-15 09:30:51 +0000

    fix: include -e to consider \n

--- tool/generate_test_optimizer_bundle.sh
+++ tool/generate_test_optimizer_bundle.sh
@@ -3,6 +3,6 @@ BUNDLE_OUTPUT_DIR="lib/src/commands/test/templates"
 mason bundle --source path ./bricks/test_optimizer/ -t dart --output-dir $BUNDLE_OUTPUT_DIR
 
 input="lib/src/commands/test/templates/test_optimizer_bundle.dart"
-echo "// To generate this file, run: tool/generate_test_optimizer_bundle.sh\n$(cat $input)" > $input
+echo -e "// To generate this file, run: tool/generate_test_optimizer_bundle.sh\n$(cat $input)" > $input
 
 dart format $BUNDLE_OUTPUT_DIR
