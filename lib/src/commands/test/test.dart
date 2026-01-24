7e2201574186faed7b784564324eec5d4bdc8326
Meylis Annagurbanov <meylis.annagurbanow98@gmail.com>
2026-01-24 15:19:52 +0500

    feat: add --collect-coverage-from option to include untested files in coverage

--- lib/src/commands/test/test.dart
+++ lib/src/commands/test/test.dart
@@ -17,6 +17,7 @@ class FlutterTestOptions {
     required this.excludeTags,
     required this.tags,
     required this.excludeFromCoverage,
+    required this.collectCoverageFrom,
     required this.randomSeed,
     required this.optimizePerformance,
     required this.updateGoldens,
@@ -38,6 +39,11 @@ class FlutterTestOptions {
     final excludeTags = argResults['exclude-tags'] as String?;
     final tags = argResults['tags'] as String?;
     final excludeFromCoverage = argResults['exclude-coverage'] as String?;
+    final collectCoverageFromString =
+        argResults['collect-coverage-from'] as String? ?? 'imports';
+    final collectCoverageFrom = CoverageCollectionMode.fromString(
+      collectCoverageFromString,
+    );
     final randomOrderingSeed =
         argResults['test-randomize-ordering-seed'] as String?;
     final randomSeed = randomOrderingSeed == 'random'
@@ -60,6 +66,7 @@ class FlutterTestOptions {
       excludeTags: excludeTags,
       tags: tags,
       excludeFromCoverage: excludeFromCoverage,
+      collectCoverageFrom: collectCoverageFrom,
       randomSeed: randomSeed,
       optimizePerformance: optimizePerformance,
       updateGoldens: updateGoldens,
@@ -89,6 +96,9 @@ class FlutterTestOptions {
 
   /// A glob which will be used to exclude files that match from the coverage.
   final String? excludeFromCoverage;
+
+  /// How to collect coverage.
+  final CoverageCollectionMode collectCoverageFrom;
 
   /// The seed to randomize the execution order of test cases within test files.
   final String? randomSeed;
@@ -134,6 +144,7 @@ typedef FlutterTestCommand =
       bool optimizePerformance,
       double? minCoverage,
       String? excludeFromCoverage,
+      CoverageCollectionMode collectCoverageFrom,
       String? randomSeed,
       bool? forceAnsi,
       List<String>? arguments,
@@ -201,6 +212,15 @@ class TestCommand extends Command<int> {
       ..addOption(
         'min-coverage',
         help: 'Whether to enforce a minimum coverage percentage.',
+      )
+      ..addOption(
+        'collect-coverage-from',
+        help:
+            'Whether to collect coverage from imported files only or all '
+            'files.',
+        allowed: ['imports', 'all'],
+        defaultsTo: 'imports',
+        valueHelp: 'imports|all',
       )
       ..addOption(
         'test-randomize-ordering-seed',
@@ -311,6 +331,7 @@ This command should be run from the root of your Flutter project.''');
               options.collectCoverage || options.minCoverage != null,
           minCoverage: options.minCoverage,
           excludeFromCoverage: options.excludeFromCoverage,
+          collectCoverageFrom: options.collectCoverageFrom,
           randomSeed: options.randomSeed,
           forceAnsi: options.forceAnsi,
           arguments: [
