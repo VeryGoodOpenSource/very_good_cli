e53ccfb5101db4ade824b76bff47f7ddc46d70ba
Erick Zanardo <erickzanardoo@gmail.com>
2026-03-06 14:11:13 -0300

    feat: Adding very_good create app_ui command

--- lib/src/commands/create/templates/templates.dart
+++ lib/src/commands/create/templates/templates.dart
@@ -1,5 +1,6 @@
 export 'post_generate_actions.dart';
 export 'template.dart';
+export 'very_good_app_ui/very_good_app_ui.dart';
 export 'very_good_core/very_good_core.dart';
 export 'very_good_dart_cli/very_good_dart_cli.dart';
 export 'very_good_dart_package/very_good_dart_package.dart';
